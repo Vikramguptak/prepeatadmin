0b0614d7ee9ac9278b0e9cd56e38f1a61a8dcd9c
Vikramguptak <Vikramguptak>
2023-10-23 16:00:14 +0000

    Pushed from Locofy Builder

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -418,6 +418,76 @@ const ContactUs = () => {
 };
 
 export default ContactUs;
+import ContactInfoContainer from "../components/ContactInfoContainer";
+import SubjectSelectorContainer from "../components/SubjectSelectorContainer";
+import styles from "./ContactUs.module.css";
+
+const ContactUs = () => {
+  return (
+    <div className={styles.contactUs}>
+      <div className={styles.contactUsParent}>
+        <b className={styles.contactUs1}>Contact Us</b>
+        <div className={styles.anyQuestionOr}>
+          Any question or remarks? Just write us a message!
+        </div>
+      </div>
+      <div className={styles.groupParent}>
+        <ContactInfoContainer />
+        <div className={styles.vectorParent}>
+          <img className={styles.groupChild} alt="" src="/vector-4.svg" />
+          <img className={styles.groupItem} alt="" src="/vector-8.svg" />
+          <div className={styles.firstName}>First Name</div>
+        </div>
+        <div className={styles.vectorGroup}>
+          <img className={styles.groupChild} alt="" src="/vector-4.svg" />
+          <div className={styles.firstName}>Email</div>
+        </div>
+        <div className={styles.vectorContainer}>
+          <img className={styles.groupChild} alt="" src="/vector-4.svg" />
+          <div className={styles.firstName}>Last Name</div>
+          <div className={styles.doe}>Doe</div>
+        </div>
+        <div className={styles.groupDiv}>
+          <img className={styles.groupChild1} alt="" src="/vector-9.svg" />
+          <div className={styles.firstName}>Message</div>
+          <div className={styles.writeYourMessage}>Write your message..</div>
+        </div>
+        <div className={styles.groupWrapper}>
+          <div className={styles.vectorParent1}>
+            <img className={styles.groupChild} alt="" src="/vector-7.svg" />
+            <div className={styles.firstName}>Phone Number</div>
+            <div className={styles.div}>+1 012 3456 789</div>
+          </div>
+        </div>
+        <SubjectSelectorContainer />
+        <div className={styles.sendMessageWrapper}>
+          <div className={styles.sendMessage}>Send Message</div>
+        </div>
+      </div>
+      <img
+        className={styles.letterSend1Icon}
+        alt=""
+        src="/letter-send-1@2x.png"
+      />
+      <img
+        className={styles.socialIconAndLogo}
+        alt=""
+        src="/social-icon-and-logo.svg"
+      />
+      <div className={styles.tc}>{`T&C`}</div>
+      <div className={styles.about}>About</div>
+      <div className={styles.contactUs2}>Contact Us</div>
+      <div className={styles.home}>Home</div>
+      <div className={styles.top}>
+        <div className={styles.logo}>
+          <img className={styles.asset21} alt="" src="/asset-2-1@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
 import styles from "./ContactUs.module.css";
 
 const ContactUs = () => {
